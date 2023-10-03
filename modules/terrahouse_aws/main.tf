79ef8aa312c9795d7d349cb0ce30815eaa629593
Alechenu Sunday <Sundaygreat13@gmail.com>
2023-10-03 01:46:22 +0000

    #26 Refactor s3 bucket into terrahouse module

---
+++ modules/terrahouse_aws/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+
+#https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
+
+
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    User_uuid = var.user_uuid
+
+  }
+}
