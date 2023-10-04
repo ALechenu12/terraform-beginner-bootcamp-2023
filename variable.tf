2d9833d14205b7c17b4c4f8db2abbc327f08f32c
ALechenu12 <139260147+ALechenu12@users.noreply.github.com>
2023-10-04 04:13:06 +0100

    #28 enables static website hosting, uploaded index and error file via terraform (#29)

--- variable.tf
+++ variable.tf
@@ -7,3 +7,15 @@ variable "bucket_name" {
   description = "The AWS S3 bucket name"
   type        = string
 }
+
+variable "index_html_filepath" {
+  description = "Path to the index.html file"
+  type     = string
+
+}
+
+variable "error_html_filepath" {
+  description = "Path to the error.html file"
+  type     = string
+
+}
