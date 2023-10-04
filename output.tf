3dcd6295bbadc3eee255818b3acc7960053725f2
Alechenu Sunday <Sundaygreat13@gmail.com>
2023-10-04 03:11:50 +0000

    #28 enables static website hosting, uploaded index and error file via terraform

--- output.tf
+++ output.tf
@@ -2,3 +2,9 @@ output "bucket_name" {
   description = "Bucket name for our static website"
   value = module.terrahouse_aws.bucket_name
 }
+
+
+output "s3website_endpoint" {
+  description = "S3 static website hoosting endpoint"
+  value = module.terrahouse_aws.website_endpoint
+}
