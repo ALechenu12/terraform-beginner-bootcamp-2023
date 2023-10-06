329a217e3167e50a80c9bd65bbf8d42bb8747861
Alechenu Sunday <Sundaygreat13@gmail.com>
2023-10-06 23:00:23 +0000

    #36 assets_path configuration

--- modules/terrahouse_aws/resources-storage.tf
+++ modules/terrahouse_aws/resources-storage.tf
@@ -44,7 +44,7 @@ resource "aws_s3_object" "index_html" {
 
 }
 resource "aws_s3_object" "upload_assets" {
-  for_each = fileset(var.assets_path,"*.{jpg,png,gif}")
+  for_each = fileset(var.assets_path,"*")
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "assets/${each.key}"
   source = "${var.assets_path}/${each.key}"
